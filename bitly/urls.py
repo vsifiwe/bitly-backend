eca7024f12171aa5aeb2ed050b24a34bfe4c763c
Manzi <asifiwemanzi@gmail.com>
2022-04-02 15:57:02 +0200

    Add Google auth

--- bitly/urls.py
+++ bitly/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('social/', include('social_auth.urls'))
 ]
