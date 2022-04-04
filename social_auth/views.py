1b26c699b68654cd768f301ce3619f2ffd6783eb
Manzi <asifiwemanzi@gmail.com>
2022-04-04 09:43:48 +0200

    add facebook auth

--- social_auth/views.py
+++ social_auth/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
-from .serializers import GoogleSocialAuthSerializer
+from .serializers import GoogleSocialAuthSerializer, FacebookSocialAuthSerializer
 from rest_framework import status
 
 class GoogleSocialAuthView(GenericAPIView):
@@ -13,6 +13,22 @@ class GoogleSocialAuthView(GenericAPIView):
 
 
         POST with "auth_token"\nfrom google to get user information
+
+        """
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = ((serializer.validated_data)['auth_token'])
+        return Response(data, status=status.HTTP_200_OK)
+
+class FacebookSocialAuthView(GenericAPIView):
+
+    serializer_class = FacebookSocialAuthSerializer
+
+    def post(self, request):
+        """
+
+        POST with "auth_token"\nfrom facebook to get user information
 
         """
 
