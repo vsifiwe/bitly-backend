9b8b664fb3167f4d9c59e59478baa4ce49f81b41
Manzi Asifiwe <asifiwemanzi@gmail.com>
2022-07-20 18:26:45 +0200

    fix url

--- api/views.py
+++ api/views.py
@@ -1,5 +1,4 @@
 import csv
-import re
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
