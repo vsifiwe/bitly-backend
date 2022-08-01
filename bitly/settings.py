6309e7d594cdba82025290ea30447cddccb30a73
Manzi Asifiwe <50745253+vsifiwe@users.noreply.github.com>
2022-08-01 06:55:13 +0200

    allow new url in cors

--- bitly/settings.py
+++ bitly/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://shorturl-amalitech.netlify.app"
+    "https://shorturl-amalitech.netlify.app",
+    "https://dashboard.amalitech.me/"
 ]
 
 AUTH_USER_MODEL = 'authentication.User'
